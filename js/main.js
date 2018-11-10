ff00f79b9b782b6fcfde08429ea77fbc402e2003
JamisonOrdway <jamisonordway@gmail.com>
2018-11-10 12:34:50 -0700

    improves basic styling

--- js/main.js
+++ js/main.js
@@ -4,14 +4,23 @@ function getBooks(){
   fetch(`https://www.googleapis.com/books/v1/volumes?q=${search_term}`)
     .then((res) => res.json())
     .then((data) => {
-      let output = '<h2>Books</h2>';
+      let output = '<h2>Search results:</h2>';
       data.items.forEach(function(book) {
         output += `
           <div id="bookInfo">
-            <h2>${book.volumeInfo.title}</h3>
-            <h3>${book.volumeInfo.authors}</h3>
-            <h3>${book.volumeInfo.publisher}</h3>
-            <a href="${book.volumeInfo.infoLink}">More info</a>
+            <div id="title">
+              <h2>Volume Title: </h2>
+              <p>${book.volumeInfo.title}</p>
+            </div>
+            <div id="author">
+              <h3>Volume Author(s): </h3>
+              <p>${book.volumeInfo.authors}</p>
+            </div>
+            <div id="otherInfo">
+              <h3>Volume Publisher:</h3>
+              <p>${book.volumeInfo.publisher}</p>
+              <a href="${book.volumeInfo.infoLink}">More info</a>
+            </div>
           </div>
         `;
       });
