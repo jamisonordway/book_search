24f0d0bb3a6fc2ce6d4172090018a9dafbad61da
JamisonOrdway <jamisonordway@gmail.com>
2018-11-10 14:12:25 -0700

    implements client side script with express server

--- server.js
+++ server.js
@@ -4,20 +4,19 @@ const app = express();
 app.set('view engine', 'ejs');
 app.set('port', process.env.PORT || 8000)
 app.listen(3000);
-app.use(express.json());
+app.use(express.static(path.join(__dirname, 'client')))
 
 app.listen(app.get('port'), () => {
   console.log(`app is listening on ${app.get('port')}`)
 })
 
 app.get('/', function(req, res) {
-  var results = getBooks()
   res.render('index')
 })
-
 
 app.get('*', function(req, res) {
   res.render('error');
 });
+
 
 module.exports = app;
